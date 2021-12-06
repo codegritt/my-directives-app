5548f4d2727cecf903ac49d09300ae58e6020795
Gokul <gokulchandan18@gmail.com>
2021-12-06 21:05:38 +0530

    commit

--- src/app/unless.directive.ts
+++ src/app/unless.directive.ts
@@ -4,7 +4,7 @@ import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
   selector: '[appUnless]'
 })
 export class UnlessDirective {
-@Input() set unless(condition:boolean){
+@Input() set appunless(condition:boolean){
   if (!condition){
     this.vcRef.createEmbeddedView(this.templateRef);
   } else{
